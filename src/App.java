72a7c902c5d0d772ca79f9b8462f34a3b36f02fd
fsannicolo <federico.sannicolo@marconirovereto.it>
2026-01-22 12:08:39 +0100

    feat:circular list's methods

--- src/App.java
+++ src/App.java
@@ -1,35 +1,76 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        CustomList list = new CustomList(new Node(5));
-        list.print();
-        //list.printRec();
+        // CustomList list = new CustomList(new Node(5));
+        // list.print();
+        // //list.printRec();
 
-        Node n1 = new Node(4);
-        Node n2 = new Node(8);
-        n1.setNext(n2); 
+        // Node n1 = new Node(4);
+        // Node n2 = new Node(8);
+        // n1.setNext(n2); 
         
-        list.append(n1);
-        list.print();  
+        // list.append(n1);
+        // list.print();  
 
-        Node n3 = new Node(0);
-        Node n4 = new Node(67);
+        // Node n3 = new Node(0);
+        // Node n4 = new Node(67);
 
-        list.add(0, n3);
-        list.add(2, n4);
+        // list.add(0, n3);
+        // list.add(2, n4);
+        // list.print();
+
+        // System.out.println(list.get(4));
+
+        // Node r = list.remove(2);
+        // System.out.println("Nodo rimosso: " + r);
+
+        // // boolean res = list.remove(n4);
+        // // System.out.println("Nodo rimosso? " + res);
+        // list.print();
+
+        // Node n5 = new Node(-3);
+        // list.set(2, n5);
+        // list.print();
+
+        CircularList list = new CircularList();
+        Node n1 = new Node(7);
+        Node n2 = new Node(8);
+        Node n3 = new Node(9);
+        Node n4 = new Node(10);
+        Node n5 = new Node(11);
+        
+        list.add(n1);
+        list.add(n2);
+        list.add(n3);
+        list.add(n4);
+        list.add(n5);
         list.print();
 
-        System.out.println(list.get(4));
+        //list.cut(3);
+        //list.print();
 
-        Node r = list.remove(2);
-        System.out.println("Nodo rimosso: " + r);
+        //int removed = list.removeRange(0, 3);
+        //System.out.printf("Nodes removed: %d \n", removed); 
+        //list.print();
 
-        // boolean res = list.remove(n4);
-        // System.out.println("Nodo rimosso? " + res);
+        Node n6 = new Node(-1);
+        Node n7 = new Node(-2);
+
+        list.newRing(n6, n7);
         list.print();
 
-        Node n5 = new Node(-3);
-        list.set(2, n5);
+        list.newRing(n2, n4);
         list.print();
+
+        list.newRing(n6, n4);
+        list.print();
+
+        list.newRing(n2, n7);
+        list.print();
+
+        System.out.println("----");
+        CircularList list2 = new CircularList();
+        list2.newRing(n6, n7);
+        list2.print();
     }
 }
--- src/CircularList.java
+++ src/CircularList.java
@@ -72,19 +72,94 @@ public class CircularList {
 
         return getRec(head, index);
     }
+
+    public boolean contains(Node n) {
+
+        return contains(head, n);
+    }
+    
+    private boolean contains(Node cursor, Node n) {
+        
+        // exit clauses
+        if (cursor == null) return false;
+        if (cursor.equals(n)) return true;
+        if (cursor == tail) return false;
+
+        // recursive call
+        return contains(cursor.getNext(), n);
+    }
     // ------------------------------------------------
 
+    /**
+     * Checks whether the list is actually circular, meaning it doesn't have null pointers
+     * @return true if it's circular, false otherwise
+     */
     public boolean checkRing() {
-        return false;
+        
+        if (this.head == null) return false;
+
+        // sweeps the list
+        Node cursor = head;
+
+        while (cursor != tail) {
+            if (cursor == null) return false;
+            cursor = cursor.getNext();
+        }
+
+        // checks the final reference
+        if (tail.getNext() != head) return false;
+
+        return true;
     }
 
-    public int removeRange(int start, int end) {
+    /**
+     * Removes every node included in the interval (excluding the bounds)
+     * @param start first index
+     * @param end last index
+     * @return number of nodes removes
+     * @throws IllegalArgumentException
+     */
+    public int removeRange(int start, int end) throws IllegalArgumentException {
 
-        return 0;
+        // wrong direction
+        if (start > end || start == end) return 0;
+
+        if (head == null) return 0;
+
+        if (start < 0 || end < 0 || start >= size() || end >= size()) 
+            throw new IllegalArgumentException("Indexes must be not negative and can't exceed length!");
+
+        // "cuts" the internal nodes
+        Node startNode = get(start);
+        Node endNode = get(end);
+        startNode.setNext(endNode); 
+
+        return end - start - 1; 
     }
 
+    /**
+     * Removes every node subsequent to a node identified by its index
+     * @param last the very last node to survive
+     */
     public void cut(int last) {
 
+        if (head == null) return;
+
+        if (last < 0) return;
+
+        // if the index exceeds the length, I keep the remaining part (only because it's circular!!)
+        last = last % size();
+
+        // I keep just one node
+        if (last == 0) {
+            // head remains the same
+            tail = head;
+            head.setNext(head);  
+        }
+
+        Node cursor = get(last);
+        cursor.setNext(head);
+        tail = cursor;
     }
 
     public Node getPrev(Node n) {
@@ -92,20 +167,65 @@ public class CircularList {
         return null;
     }
 
+    /**
+     * Specifies the new starting and ending nodes of the list, if they are present. If not, they are added
+     * as the new head and tail respectively
+     * @param n1 new head node
+     * @param n2 new tail node
+     */
     public void newRing(Node n1, Node n2) {
 
+        if (n1 == null || n2 == null) throw new NullPointerException("Nodes can't be null!");
+
+        // if the nodes are equal
+        if (n1 == n2) {
+            head = n1;
+            tail = n2;
+            head.setNext(tail);
+            tail.setNext(head);
+            return;
+        }
+
+        // checks the first node
+        if (contains(n1))
+            head = n1;
+        else { // and if head==null...
+            n1.setNext(head);
+            head = n1;
+        }
+
+        // if there's no tail yet, I'll end up with just 2 nodes
+        if (tail == null) {
+            head.setNext(n2);
+            tail = n2;
+            tail.setNext(head);
+            return;
+        }
+
+        // checks the second node
+        if (contains(n2))
+            tail = n2;
+        else {
+            tail.setNext(n2);
+            tail = n2;
+        }
+
+        // resets the last reference
+        tail.setNext(head);
     }
 
     public void print() {
 
         if (head == null) return; 
 
+        System.out.print('[');
         Node cursor = head;
         do {
-            System.out.println(cursor.toString());
+            System.out.print(cursor.toString());
             cursor = cursor.getNext();
         }
         while (cursor != head);
+        System.out.println(']');
     }
 }
 
